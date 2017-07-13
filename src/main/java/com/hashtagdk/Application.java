41a74e132c317773b8e57a8ed6587dd64befb707
HashTagDK <sawy3r11@gmail.com>
2017-07-13 23:24:34 +0200

    Start

---
+++ src/main/java/com/hashtagdk/Application.java
@@ -0,0 +1,14 @@
+package com.hashtagdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dawid on 7/13/17.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
